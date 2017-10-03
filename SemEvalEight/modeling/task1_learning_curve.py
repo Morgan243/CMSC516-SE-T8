7f45cd34b7922083f4c57ab0cffdb1ce3fdb4282
fatima <fmasche@gmail.com>
2017-10-02 22:12:05 -0400

    Adding method learning_curve_dt. 1. Load a train portion and test portion of the dataset. Use about 70% of the data for train, and the rest for testing 2. Create a loop where you increment the amount of training data provided to the model. So for instance, say you have 1k training samples. On the first loop, the model trains on 100 random samples, on the second loop it trains on the previous 100, plus a new set of 100, making 200 (each loops training dataset is a superset of the previous loop). 3. In each loop, after the model is fit to the subset of data, test the models predictive ability on the test dataset. So run predict on the test dataset you loaded, compute the metrics using utils.binary_classification_metrics, then save those result to a list holding results for each loop 4. Loop all the way up to where the model gets to see all of the training data. 5. Once it's done training/testing on each subset, compile/save all the results, then visualize the curve where the X axis is the proportion of the training data and the y-axis is the performance on the test data (usually accuracy or F1-score).

--- SemEvalEight/modeling/task1_learning_curve.py
+++ SemEvalEight/modeling/task1_learning_curve.py
@@ -10,6 +10,69 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 from SemEvalEight.data_prep.loaders import load_subtask1_data
 from SemEvalEight.config import tokenized_dir
+from SemEvalEight import utils
+
+def learning_curve_dt(X, y, X_test, y_test):
+    """
+
+    :param X: Training dataset.
+    :param y: Target relative to X.
+    :param X_test: Testing dataset.
+    :param y_test: Target relative to X_test.
+    :return: Dictionary where key = total training samples, and value = metrics from test.
+    """
+
+    dt = DecisionTreeClassifier()
+    results = dict()
+    X_length = len(X)
+    X_length_inc = X_length // 10
+    X_length_count = X_length_inc
+
+    for x in range(0, 10):
+        print("This is the %s iteration" % str(x + 1))
+        print("Total training samples is %s" % str(X_length_count))
+
+        X_tmp = X[0:X_length_count]
+        y_tmp = y[0:X_length_count]
+
+        dt.fit(X_tmp, y_tmp)
+        y_predicted = dt.predict(X_test)
+
+        metrics = utils.binary_classification_metrics(y_test, y_predicted)
+        print(metrics)
+
+        results.update({str(X_length_count):metrics})
+        X_length_count = X_length_count + X_length_inc
+
+    print("Plotting results...")
+
+    samples_range = []
+    accuracy = []
+    f1_score = []
+
+    for key, value in results.items():
+        samples_range.append(key)
+        accuracy.append(value["acc"])
+        f1_score.append(value["f1"])
+
+    plt.figure()
+    plt.title("Learning curve - Decision tree")
+    plt.xlabel("Training samples")
+    plt.ylabel("Accuracy")
+    plt.plot(samples_range, accuracy)
+
+
+    plt.figure()
+    plt.title("Learning curve - Decision tree")
+    plt.xlabel("Training samples")
+    plt.ylabel("F1-score")
+    plt.plot(samples_range, f1_score)
+
+    plt.show()
+
+    print(results)
+    return results
+
 
 
 def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
@@ -86,48 +149,66 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
     plt.legend(loc="best")
     return plt
 
-#digits = load_digits()
-#X, y = digits.data, digits.target
+# #digits = load_digits()
+# #X, y = digits.data, digits.target
+#
+# file_ixs = list(range(39))
+# X, y = load_subtask1_data(file_ixs[:20],
+#                           tokenized_folder=tokenized_dir)
+# #X_test, Y_test = load_subtask1_data(file_ixs[20:],
+# #                                    tokenized_folder=tokenized_dir)
+#
+# cvec = CountVectorizer(ngram_range=(1, 2))
+#
+# cvec_X = cvec.fit_transform(X)
+# cvec_X = cvec_X.toarray()
+#
+# #cvec_X_test = cvec.transform(X_test)
+# #cvec_X_test = cvec_X_test.toarray()
+#
+# #cv_kwargs = dict(n_jobs=n_jobs,
+# #                 X=cvec_X, Y=Y)
+#
+#
+# title = "Learning Curves (Naive Bayes)"
+# # Cross validation with 100 iterations to get smoother mean test and train
+# # score curves, each time with 20% data randomly selected as a validation set.
+# cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
+# estimator = GaussianNB()
+# print("Starting plot learning curve - GaussianNB")
+# plot_learning_curve(estimator, title, cvec_X, y, ylim=(0.7, 1.01), cv=cv, n_jobs=1)
+#
+# title = "Learning Curves (SVM, RBF kernel, $\gamma=0.001$)"
+# # SVC is more expensive so we do a lower number of CV iterations:
+# # cv = ShuffleSplit(n_splits=1, test_size=0.2, random_state=0)
+# # estimator = SVC(gamma=0.001)
+# # print("Starting plot learning curve - SVC")
+# # plot_learning_curve(estimator, title, cvec_X, y, (0.7, 1.01), cv=cv, n_jobs=1)
+#
+#
+# title = "Learning Curves (Decision Tree)"
+# cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
+# estimator = DecisionTreeClassifier()
+# print("Starting plot learning curve - DecisionTree")
+# plot_learning_curve(estimator, title, cvec_X, y, (0.7, 1.01), cv=cv, n_jobs=1)
+#
+# print("Let's show it")
+# plt.show()
 
+
+# total of 39
 file_ixs = list(range(39))
-X, y = load_subtask1_data(file_ixs[:20],
+X, y = load_subtask1_data(file_ixs[:28],
                           tokenized_folder=tokenized_dir)
-#X_test, Y_test = load_subtask1_data(file_ixs[20:],
-#                                    tokenized_folder=tokenized_dir)
+X_test, y_test = load_subtask1_data(file_ixs[28:],
+                                    tokenized_folder=tokenized_dir)
 
 cvec = CountVectorizer(ngram_range=(1, 2))
 
 cvec_X = cvec.fit_transform(X)
 cvec_X = cvec_X.toarray()
 
-#cvec_X_test = cvec.transform(X_test)
-#cvec_X_test = cvec_X_test.toarray()
+cvec_X_test = cvec.transform(X_test)
+cvec_X_test = cvec_X_test.toarray()
 
-#cv_kwargs = dict(n_jobs=n_jobs,
-#                 X=cvec_X, Y=Y)
-
-
-title = "Learning Curves (Naive Bayes)"
-# Cross validation with 100 iterations to get smoother mean test and train
-# score curves, each time with 20% data randomly selected as a validation set.
-cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
-estimator = GaussianNB()
-print("Starting plot learning curve - GaussianNB")
-plot_learning_curve(estimator, title, cvec_X, y, ylim=(0.7, 1.01), cv=cv, n_jobs=1)
-
-title = "Learning Curves (SVM, RBF kernel, $\gamma=0.001$)"
-# SVC is more expensive so we do a lower number of CV iterations:
-# cv = ShuffleSplit(n_splits=1, test_size=0.2, random_state=0)
-# estimator = SVC(gamma=0.001)
-# print("Starting plot learning curve - SVC")
-# plot_learning_curve(estimator, title, cvec_X, y, (0.7, 1.01), cv=cv, n_jobs=1)
-
-
-title = "Learning Curves (Decision Tree)"
-cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
-estimator = DecisionTreeClassifier()
-print("Starting plot learning curve - DecisionTree")
-plot_learning_curve(estimator, title, cvec_X, y, (0.7, 1.01), cv=cv, n_jobs=1)
-
-print("Let's show it")
-plt.show()
+learning_curve_dt(cvec_X, y, cvec_X_test, y_test)
