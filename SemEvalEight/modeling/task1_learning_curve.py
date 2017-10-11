8794eeb976a18aa32f2bbcdf4312b6d40db6776a
fatima <fmasche@gmail.com>
2017-10-11 11:10:27 -0400

    Adding comments.

--- SemEvalEight/modeling/task1_learning_curve.py
+++ SemEvalEight/modeling/task1_learning_curve.py
@@ -17,6 +17,11 @@ from SemEvalEight import utils
 
 def calculate_learning_curve(classifier, X, y, X_test, y_test):
     """
+    -- fatima
+    Generates a plot of the learning curve with the test data (F1 score and standard deviation)
+    Iterates through the training samples 100 times, and each time the sample gradually increases.
+    For each number of samples, make a 3-fold cross-validation. The mean is used for the graphic.
+
 
     :param classifier: Object type that implements the "fit" and "predict" methods.
     :param X: Training dataset.
@@ -35,12 +40,14 @@ def calculate_learning_curve(classifier, X, y, X_test, y_test):
         print("This is the %s iteration" % str(x + 1))
         print("Total training samples is %s" % str(number_samples))
 
+        # We shuffle the training data on each iteration
         X, y = shuffle(X, y, random_state=0)
 
         partial_results = []
         for r in range(0, 3):
             #X_tmp, y_tmp = resample(X, y, n_samples=X_length_count, random_state=0)
 
+            # Generates a random array of index numbers to take it from the training set.
             idx = np.random.choice(np.arange(len(X)), number_samples, replace=False)
             X_tmp = X[idx,:]
             y_tmp = np.take(y, idx)
@@ -51,15 +58,17 @@ def calculate_learning_curve(classifier, X, y, X_test, y_test):
             metrics = utils.binary_classification_metrics(y_test, y_predicted)
             print(metrics)
 
+            # We save the F1 score to calculate the mean
             partial_results.append(metrics["f1"])
 
-
+        # Calculate mean and sdt from k-cross
         mean_f1 = np.mean(partial_results)
         print("The mean is -> ", mean_f1)
 
         sdt_f1 = np.std(partial_results, ddof=1)
         print("The sdt is -> ", sdt_f1)
 
+        # Add results to array to generate plot at the end
         results.update({str(number_samples):dict(mean_f1=mean_f1, sdt_f1=sdt_f1)})
         number_samples = number_samples + X_length_inc
 
@@ -74,6 +83,7 @@ def calculate_learning_curve(classifier, X, y, X_test, y_test):
         mean_f1.append(value["mean_f1"])
         sdt_f1.append(value["sdt_f1"])
 
+    # Configure plot settings
     plt.figure(figsize=(12, 9))
     ax = plt.subplot(111)
     ax.spines["top"].set_visible(False)
@@ -102,82 +112,8 @@ def calculate_learning_curve(classifier, X, y, X_test, y_test):
 
 
 
-def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
-                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
-    """
-    Generate a simple plot of the test and training learning curve.
-
-    Parameters
-    ----------
-    estimator : object type that implements the "fit" and "predict" methods
-        An object of that type which is cloned for each validation.
-
-    title : string
-        Title for the chart.
-
-    X : array-like, shape (n_samples, n_features)
-        Training vector, where n_samples is the number of samples and
-        n_features is the number of features.
-
-    y : array-like, shape (n_samples) or (n_samples, n_features), optional
-        Target relative to X for classification or regression;
-        None for unsupervised learning.
-
-    ylim : tuple, shape (ymin, ymax), optional
-        Defines minimum and maximum yvalues plotted.
-
-    cv : int, cross-validation generator or an iterable, optional
-        Determines the cross-validation splitting strategy.
-        Possible inputs for cv are:
-          - None, to use the default 3-fold cross-validation,
-          - integer, to specify the number of folds.
-          - An object to be used as a cross-validation generator.
-          - An iterable yielding train/test splits.
-
-        For integer/None inputs, if ``y`` is binary or multiclass,
-        :class:`StratifiedKFold` used. If the estimator is not a classifier
-        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
-
-        Refer :ref:`User Guide <cross_validation>` for the various
-        cross-validators that can be used here.
-
-    n_jobs : integer, optional
-        Number of jobs to run in parallel (default 1).
-    """
-    plt.figure()
-    plt.title(title)
-    if ylim is not None:
-        plt.ylim(*ylim)
-    plt.xlabel("Training examples")
-    plt.ylabel("Score")
-
-    train_sizes, train_scores, test_scores = learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
-
-    train_scores_mean = np.mean(train_scores, axis=1)
-
-    train_scores_std = np.std(train_scores, axis=1)
-
-    test_scores_mean = np.mean(test_scores, axis=1)
-
-    test_scores_std = np.std(test_scores, axis=1)
-
-    plt.grid()
-
-    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                     train_scores_mean + train_scores_std, alpha=0.1,
-                     color="r")
-    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
-    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-             label="Training score")
-    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-             label="Cross-validation score")
-
-    plt.legend(loc="best")
-    return plt
-
-
 # total of 39
+# we took 70% of the files for training, and 30% for testing.
 file_ixs = list(range(39))
 X, y = load_subtask1_data(file_ixs[:28],
                           tokenized_folder=tokenized_dir)
